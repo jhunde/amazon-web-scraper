f445be296865926d573956c91bb7418b737580d6
hunde <jena.kelbessa@gmail.com>
2025-09-09 00:00:01 -0700

    adjusted main code, need to fix error

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ driver = webdriver.Chrome()
 
 url = "https://www.amazon.com/"
 driver.get(url)
+
+def get_url(search_item):
+    # generate url from seach term
+    #   test a different approach here 
+    template = "https://www.amazon.com/s?k={}&crid=2H2VCCND18SAD&sprefix=tv%2Caps%2C613&ref=nb_sb_noss_1"
+    search_item = search_item.replace(' ', '+')  # using '+' char in the place of an empty space char 
+    return template.format(search_item)
+
+# test
+url = get_url("tv")
+print 
